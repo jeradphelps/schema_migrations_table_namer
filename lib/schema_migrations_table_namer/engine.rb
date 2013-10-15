36539a31e031ac0616afa910eb272745574adb3c
Jerad Phelps <jerad.phelps@gmail.com>
2013-10-15 13:01:27 -0500

    added rspec

--- lib/schema_migrations_table_namer/engine.rb
+++ lib/schema_migrations_table_namer/engine.rb
@@ -1,4 +1,9 @@
 module SchemaMigrationsTableNamer
   class Engine < ::Rails::Engine
+    config.generators do |g|
+      g.test_framework :rspec
+      g.integration_tool :rspec
+    end
+
   end
 end
