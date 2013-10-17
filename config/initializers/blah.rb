73a2dad339a4b8d890acfc5a328bbcfaf458ab35
Jerad Phelps <jerad.phelps@gmail.com>
2013-10-17 12:38:00 -0500

    readme

--- config/initializers/blah.rb
+++ config/initializers/blah.rb
@@ -1,7 +1,4 @@
 Rails.application.config.after_initialize do
-  puts 'inside'
-
-
   module ActiveRecord
     class Migrator
       @@original_schema_migrations_name = ActiveRecord::Migrator.schema_migrations_table_name
@@ -15,8 +12,4 @@ Rails.application.config.after_initialize do
       end
     end
   end
-
-  puts 'done inside'
 end
-
-puts 'outside'
